ed98ae100c8c969fc732959e2a58b85d2f53e6f3
rjs24 <rjseacome@gmail.com>
2019-12-18 16:01:59 +0000

    minor packaging correction for spider starter script

--- allrecipes/spider_starter.py
+++ allrecipes/spider_starter.py
@@ -1,6 +1,6 @@
 from scrapy.crawler import CrawlerProcess
 from scrapy.utils.project import get_project_settings
-from .spiders.recipe_crawler import RecipeCrawlerSpider
+from allrecipes.spiders.recipe_crawler import RecipeCrawlerSpider
 
 process = CrawlerProcess(get_project_settings())
 process.crawl(RecipeCrawlerSpider)
